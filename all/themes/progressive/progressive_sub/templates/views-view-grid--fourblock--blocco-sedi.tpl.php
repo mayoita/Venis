10363e6fc30cb9ad07056a6c0e325dd385dbcb3f
Massimo Moro <mayoita@hotmail.com>
2017-11-10 17:29:22 +0100

    Last

---
+++ all/themes/progressive/progressive_sub/templates/views-view-grid--fourblock--blocco-sedi.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: massimomoro
+ * Date: 02/08/17
+ * Time: 11:15
+ */
+?>
+
+<section class="event_list">
+    <?php foreach ($rows as $row_number => $columns): ?>
+        <?php foreach ($columns as $column_number => $item): ?>
+                    <?php print $item; ?>
+        <?php endforeach; ?>
+    <?php endforeach; ?>
+</section>
+
